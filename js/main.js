bbeeb7db72d4d1569ab00f8cfe57ee837aaacc36
iliass <iliass24@gmail.com>
2017-04-24 14:27:54 -0400

    add unversioned files

--- js/main.js
+++ js/main.js
@@ -1,42 +1,42 @@
-(function($) {
-    // "use strict"; // Start of use strict
+(($)=> {
+    "use strict"; // Start of use strict
 
     $(window).scroll(()=>{
     	//navbar style when scrolling
-    	if( ($(window).scrollTop() >= 50) && ($(window).width() > 400) ){
+    	if( $(window).scrollTop() >= 50 && $(window).width() > 400 ){
 
     		$('nav').css({
 				'backgroundColor':'white',
 				'z-index':'3',
 				'font-weight':'700'
-			});
-			$('.nav-link-contacts>a ,.nav-link-portfolio>a').css({
+            });
+            $('.nav-link-contacts>a ,.nav-link-portfolio>a').css({
 					'color':'black',
 		    		'font-weight': '700'
 		    });
-			$('.navbar-brand').css({
+            $('.navbar-brand').css({
 					'color':'#F05F40',
 		    		'text-transform': 'uppercase'
 		    })
     	}
     	//navbar style when on top 
-    	else if ($(window).scrollTop() < 50){
-    		$('nav').css({
-    			'transition': 'all 0.5s',
-    			'background-color': 'transparent',
-    			'flex-flow': 'row nowrap',
-    			'justify-content': 'flex-start'
-    			,'max-height': '50px',
-    			'border-bottom': '1px solid white'
-			})
-			$('.nav-link-contacts>a ,.nav-link-portfolio>a').css({
-				'color':'#afadb0',
-	    		'text-decoration': 'none'
-		    })
-			$('.navbar-brand').css({
-				'color':'rgba(255,255,255,.7)',
-		    	'text-transform': 'uppercase'
-		    })
+    	else if ( $(window).scrollTop() < 50 ) {
+				$('nav').css({
+					'transition': 'all 0.5s',
+					'background-color': 'transparent',
+					'flex-flow': 'row nowrap',
+					'justify-content': 'flex-start'
+					,'max-height': '50px',
+					'border-bottom': '1px solid white'
+				})
+				$('.nav-link-contacts>a ,.nav-link-portfolio>a').css({
+					'color':'#afadb0',
+					'text-decoration': 'none'
+				})
+				$('.navbar-brand').css({
+					'color':'rgba(255,255,255,.7)',
+					'text-transform': 'uppercase'
+				})
     		}
     })
 
